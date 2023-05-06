e7cf0271e69b5bcd0e3bbc255b4cc6d648c971b4
Alex Njagi <1992anjagi@gmail.com>
2023-05-06 16:52:59 +0300

    Adding and removing successful

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState((false))
+  const [msg, setMsg] = useState('add')
+  const [inner, setInner] = useState('Add to Cart')
+
+  const handleAddCart = (e) => {
+    setCart(true)
+  }
+
+  const handleMsg = (e) => {
+    setMsg('remove')
+    setInner('Remove from Cart')
+  }
+
+  const itemClass = cart ? "in-cart" : ""
+  
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+
+      <button className={msg}
+              onClick={(e)=>(
+                handleAddCart(),
+                handleMsg()
+              )}
+      >{inner}</button>
     </li>
   );
 }
